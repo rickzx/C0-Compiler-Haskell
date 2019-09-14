9b56a4230fea3745e456463c5c8249c99fb7e549
Eric Yang <ericyang@Erics-MBP-2.wv.cc.cmu.edu>
2019-09-14 16:59:38 -0400

    Livevariable and interference graph construction

--- lab1/src/Compile/Backend/LiveVariable.hs
+++ lab1/src/Compile/Backend/LiveVariable.hs
@@ -1,23 +1,24 @@
-module Compile.Backend.LiveVariable(getLoc, reverseAAsm, computeLivelist) where 
+module Compile.Backend.LiveVariable where 
 
     import Data.List
-    
-<<<<<<< HEAD:lab1/src/Compile/Backend/Live_variable.hs
+    import qualified Data.Set as Set
+    import qualified Data.Map as Map
+    import Data.Maybe
+
     import System.IO
     
     import Control.Monad.Trans.Except
     import Compile.Types.Ops
 
-=======
->>>>>>> d0ee5c2c97597f545eb93ccf7ab625bb6b119f73:lab1/src/Compile/Backend/LiveVariable.hs
     import Compile.Types
     
+    type Graph = Map.Map ALoc (Set.Set ALoc)
     --given a list of AVal, we just care about the temps, not the
     --constants.
-    getLoc :: [AVal] -> [ALoc]
-    getLoc [] = []
+    getLoc :: [AVal] -> Set.Set ALoc
+    getLoc [] = Set.empty
     getLoc (x:rest) = case x of
-        ALoc a -> a : getLoc rest
+        ALoc a -> Set.insert a (getLoc rest)
         _ -> getLoc rest
     
     --reverse the abstract assembly, so that we can work backwards for live variables
@@ -33,7 +34,81 @@ module Compile.Backend.LiveVariable(getLoc, reverseAAsm, computeLivelist) where
     {-for if else statement and loop, do a general tree stucture
         of the code by breaking control into segments, for each segment, 
         we do the single line livelist computes-}
-        
+    computeLive :: ([Set.Set ALoc], [AAsm]) -> ([Set.Set ALoc], [AAsm])
+    computeLive (accset, []) = (accset, [])
+    computeLive (accset, x:ast) = 
+        case x of 
+            ARet retval -> case retval of
+                ALoc a -> computeLive([Set.singleton a], ast)
+                AImm _ -> computeLive([Set.empty], ast)
+            AComment _ -> computeLive(accset, ast)
+            AAsm {aAssign = assign, aOp = op, aArgs = args} ->
+                case accset of 
+                    [] -> error "The last statement of AAsm is not return"
+                    y:rest -> let
+                        relev = getLoc args
+                        current = (Set.union y relev) Set.\\ (Set.fromList assign) in
+                        computeLive(current:accset, ast)
+
+    --for each set we have, all the elements in the set interferes with each other,
+    --thus, we separate the set into key value paring of each key and values interferes with it
+    --requires Set != Empty
+    findAllElem :: Set.Set ALoc -> ALoc -> (ALoc, Set.Set ALoc)
+    findAllElem liveset a = if Set.member a liveset then (a, Set.delete a liveset)
+        else let
+            _ = error "interference graph input shouldnt be empty" in (a, liveset)
+            
+    findInterfere :: Set.Set ALoc -> Graph -> Graph
+    findInterfere liveset g = let
+        --(livevar, intefer group) for each element
+        intef_set = Set.map (findAllElem liveset) liveset
+        graph = Set.foldr fold_fn g intef_set where 
+            fold_fn :: (ALoc, Set.Set ALoc) -> Graph -> Graph
+            fold_fn (a, intset) g' = 
+                case Map.lookup a g' of
+                    Just sset -> let newset = Set.union sset intset in
+                        Map.insert a newset g'      
+                    Nothing -> Map.insert a intset g'
+        in
+            graph
+
+    computeInterfere :: [Set.Set ALoc] -> Graph -> Graph
+    computeInterfere [] g = g
+    computeInterfere (x:xs) g = foldl (flip findInterfere) g xs
+
+    --example from Written 1
+    exAASM :: [AAsm]
+    exAASM = [AAsm{aAssign = [ATemp 0], aOp = ANop, aArgs = [AImm (-9)]}, 
+        AAsm{aAssign = [ATemp 1], aOp = ANop, aArgs = [AImm 1]},
+        AAsm{aAssign = [ATemp 2], aOp = ANop, aArgs = [AImm 2]},
+        AAsm{aAssign = [ATemp 3], aOp = ANop, aArgs = [AImm 3]},
+        AAsm{aAssign = [ATemp 4], aOp = AMul, aArgs = [ALoc (ATemp 2), ALoc(ATemp 3)]},
+        AAsm{aAssign = [ATemp 5], aOp = AAdd, aArgs = [ALoc (ATemp 1), ALoc(ATemp 4)]},
+        AAsm{aAssign = [ATemp 6], aOp = AAdd, aArgs = [ALoc (ATemp 0), ALoc(ATemp 5)]},
+        AAsm{aAssign = [ATemp 7], aOp = ANop, aArgs = [AImm 2]},
+        AAsm{aAssign = [ATemp 8], aOp = AAdd, aArgs = [ALoc (ATemp 6), ALoc(ATemp 7)]},
+        AAsm{aAssign = [ATemp 9], aOp = ANop, aArgs = [AImm 4]},
+        AAsm{aAssign = [ATemp 10], aOp = ADiv, aArgs = [ALoc (ATemp 8), ALoc(ATemp 9)]},
+        AAsm{aAssign = [AReg 0], aOp = AMod, aArgs = [ALoc (ATemp 6), ALoc(ATemp 10)]},
+        AComment "This should do nothing",
+        ARet (ALoc (AReg 0))]
+
+    
+    testLive :: IO ()
+    testLive = do{
+       print exAASM;
+       print (computeLive([], reverseAAsm [] exAASM))
+    }
+
+    testInterfere :: IO ()
+    testInterfere = let 
+        (livelist, _) = computeLive([], reverseAAsm [] exAASM)
+        in print(computeInterfere livelist Map.empty)
+
+
+       -- computeInterfere xs (findInterfere x g)
+
+{-
     computeLivelist :: ([[ALoc]], [AAsm]) -> ([[ALoc]], [AAsm])
     computeLivelist (accum, []) = (accum, [])
     computeLivelist (accum, x:ast) = 
@@ -50,9 +125,5 @@ module Compile.Backend.LiveVariable(getLoc, reverseAAsm, computeLivelist) where
                 relev = getLoc args
                 current_live = ((y \\ assign) ++ (relev \\ y)) in
                 computeLivelist(current_live:accum, ast)
-    
-    testAASM :: [AAsm]
-    testAASM = [AAsm{aAssign = [AReg 0], aOp = AAdd, aArgs = [AImm 5, AImm 7]}, AAsm{aAssign = [AReg 1], aOp = AAdd, aArgs = [AImm 3, AImm 5]}, AAsm{aAssign = [AReg 2], aOp = AAdd, aArgs = [ALoc (AReg 0), ALoc(AReg 1)]}, ARet (ALoc(AReg 2))]
-    
-    test1 :: IO ()
-    test1 = print (computeLivelist([], reverseAAsm [] testAASM))
+  
+-}
