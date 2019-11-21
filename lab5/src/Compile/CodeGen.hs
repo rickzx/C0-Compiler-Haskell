bec503b6301d522a11282a87c62b1355c7148f0a
Eric Yang <ericyang@Erics-MacBook-Pro-2.local>
2019-11-21 17:06:20 -0500

    strange

--- lab5/src/Compile/CodeGen.hs
+++ lab5/src/Compile/CodeGen.hs
@@ -15,6 +15,7 @@ module Compile.CodeGen where
     import Compile.Backend.SSA
     import Compile.Backend.SSAOptimize
     import Compile.Types
+    import Compile.Backend.Inline
     import qualified Data.List as List
     import qualified Data.Map as Map
     import Debug.Trace
@@ -23,7 +24,7 @@ module Compile.CodeGen where
     --codeGen t | (trace $ show t) False = undefined
     codeGen tast structs unsafe =
         let 
-            (tastGen, _tr) = aasmGen tast structs unsafe
+            (tastGen, _tr) = inlineOpt $ aasmGen tast structs unsafe
             memerr = [
                 AControl $ ALab "memerror",
                 AAsm [AReg 3] ANop [AImm 12],
@@ -35,7 +36,7 @@ module Compile.CodeGen where
     --asmGen t h | (trace $ show t ++ "\n\n" ++ show h) False = undefined
     asmGen tast header structs unsafe =
         let memerr = ".memerror:\n\tmovl $12, %edi\n\txorl %eax, %eax\n\tcall raise\n"
-            (tastGen, tr) = aasmGen tast structs unsafe
+            (tastGen, tr) = inlineOpt $ aasmGen tast structs unsafe
             globs = map (\(x, _) -> if x == "a bort" then Global "_c0_abort_local411" else Global $ "_c0_" ++ x) tastGen
             globString = concatMap (\line -> show line ++ "\n") globs
          in globString ++ concatMap (\(fn, (aasms, lv)) -> generateFunc (fn, aasms, lv) header tr) tastGen ++ memerr
